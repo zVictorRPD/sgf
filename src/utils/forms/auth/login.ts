6758f74b0dc9473ff519d64c69d4c33ef3691d1d
Victor Martins <victor2007azevedo@hotmail.com>
2024-10-11 09:52:52 -0300

    configurando primeira build

--- src/utils/forms/auth/login.ts
+++ src/utils/forms/auth/login.ts
@@ -1,8 +1,8 @@
 import * as Yup from 'yup';
 
 export const initialValues = {
-    email: 'victor2007azevedo@hotmail.com',
-    password: 'DChhwTdzp9cJ',
+    email: '',
+    password: '',
 }
 
 export const validationSchema = Yup.object({
